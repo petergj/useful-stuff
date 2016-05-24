bf53291f1fa912594cea1de57b99ef2eaf6ad288
petergj <petergj@users.noreply.github.com>
2016-05-24 14:21:48 +0200

    Increase fillfactor of indexes

--- SQL/MSSQL/Rebuild all indexes on database.sql
+++ SQL/MSSQL/Rebuild all indexes on database.sql
@@ -6,7 +6,7 @@
 DECLARE @TableName VARCHAR(255)
 DECLARE @sql NVARCHAR(500)
 DECLARE @fillfactor INT
-SET @fillfactor = 80
+SET @fillfactor = 95
 DECLARE TableCursor CURSOR FOR
 
 SELECT OBJECT_SCHEMA_NAME([object_id])+'.['+name+']' AS TableName FROM sys.tables
